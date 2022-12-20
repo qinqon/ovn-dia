3f2a8f008e299072296262369a23eb45d0448017
Enrique Llorente <ellorent@redhat.com>
2022-12-20 10:02:10 +0100

    topology: Resolve references
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- pkg/topology/nb.go
+++ pkg/topology/nb.go
@@ -9,123 +9,183 @@ import (
 	"github.com/qinqon/ovn-dia/pkg/nbdb"
 )
 
-type SwitchPorts map[string]SwitchPort
-type RouterPorts map[string]RouterPort
-type Switches map[string]Switch
-type Routers map[string]Router
+type SwitchPorts map[string]*SwitchPort
+type RouterPorts map[string]*RouterPort
+type Switches map[string]*Switch
+type Routers map[string]*Router
 
 type NorthBound struct {
-	SwitchPorts SwitchPorts
-	RouterPorts RouterPorts
-	Switches    Switches
-	Routers     Routers
+	SwitchPorts      SwitchPorts
+	RouterPorts      RouterPorts
+	RouterPortByName RouterPorts
+	Switches         Switches
+	Routers          Routers
+	RouterByName     Routers
 }
 
 type SwitchPort struct {
-	Dia *diagram.Node
-	NB  *nbdb.LogicalSwitchPort
+	Dia        *diagram.Node
+	NB         *nbdb.LogicalSwitchPort
+	Owner      *Switch
+	RouterPort *RouterPort
 }
 
 type RouterPort struct {
-	Dia *diagram.Node
-	NB  *nbdb.LogicalRouterPort
+	Dia   *diagram.Node
+	NB    *nbdb.LogicalRouterPort
+	Owner *Router
+	Peer  *Router
 }
 
 type Switch struct {
-	Dia *diagram.Node
-	NB  *nbdb.LogicalSwitch
+	Dia   *diagram.Node
+	NB    *nbdb.LogicalSwitch
+	Ports SwitchPorts
 }
 
 type Router struct {
-	Dia *diagram.Node
-	NB  *nbdb.LogicalRouter
+	Dia   *diagram.Node
+	NB    *nbdb.LogicalRouter
+	Ports RouterPorts
 }
 
 func newNorthBound() *NorthBound {
 	return &NorthBound{
-		SwitchPorts: SwitchPorts{},
-		RouterPorts: RouterPorts{},
-		Switches:    Switches{},
-		Routers:     Routers{},
+		SwitchPorts:      SwitchPorts{},
+		RouterPorts:      RouterPorts{},
+		RouterPortByName: RouterPorts{},
+		Switches:         Switches{},
+		Routers:          Routers{},
+		RouterByName:     Routers{},
 	}
 }
 
 func LoadNorthBound(ctx context.Context, cli client.Client) (*NorthBound, error) {
 	nb := newNorthBound()
-	var err error
-	nb.Switches, err = loadSwitches(ctx, cli)
-	if err != nil {
+	if err := nb.loadSwitches(ctx, cli); err != nil {
 		return nil, err
 	}
-	nb.Routers, err = loadRouters(ctx, cli)
-	if err != nil {
+
+	if err := nb.loadRouters(ctx, cli); err != nil {
 		return nil, err
 	}
-	nb.SwitchPorts, err = loadSwitchPorts(ctx, cli)
-	if err != nil {
+
+	if err := nb.loadSwitchPorts(ctx, cli); err != nil {
 		return nil, err
 	}
-	nb.RouterPorts, err = loadRouterPorts(ctx, cli)
-	if err != nil {
+
+	if err := nb.loadRouterPorts(ctx, cli); err != nil {
 		return nil, err
 	}
+
+	nb.resolveReferences()
+
 	return nb, nil
 }
 
-func loadSwitches(ctx context.Context, cli client.Client) (Switches, error) {
+func (nb *NorthBound) loadSwitches(ctx context.Context, cli client.Client) error {
 	lss := []*nbdb.LogicalSwitch{}
 	if err := cli.List(ctx, &lss); err != nil {
-		return nil, err
+		return err
 	}
-	switches := Switches{}
 	for _, ls := range lss {
-		switches[ls.UUID] = Switch{
+		nb.Switches[ls.UUID] = &Switch{
 			NB:  ls,
 			Dia: generic.Network.Switch(diagram.NodeLabel(ls.Name)),
 		}
 	}
-	return switches, nil
+	return nil
 }
 
-func loadRouters(ctx context.Context, cli client.Client) (Routers, error) {
+func (nb *NorthBound) loadRouters(ctx context.Context, cli client.Client) error {
 	lrs := []*nbdb.LogicalRouter{}
 	if err := cli.List(ctx, &lrs); err != nil {
-		return nil, err
+		return err
 	}
-	routers := Routers{}
 	for _, lr := range lrs {
-		routers[lr.UUID] = Router{
+		router := &Router{
 			NB:  lr,
 			Dia: generic.Network.Router(diagram.NodeLabel(lr.Name)),
 		}
+		nb.Routers[lr.UUID] = router
+		nb.RouterByName[lr.Name] = router
 	}
-	return routers, nil
+	return nil
 }
 
-func loadSwitchPorts(ctx context.Context, cli client.Client) (SwitchPorts, error) {
+func (nb *NorthBound) loadSwitchPorts(ctx context.Context, cli client.Client) error {
 	lsps := []*nbdb.LogicalSwitchPort{}
 	if err := cli.List(ctx, &lsps); err != nil {
-		return nil, err
+		return err
 	}
-	switchPorts := SwitchPorts{}
 	for _, lsp := range lsps {
-		switchPorts[lsp.UUID] = SwitchPort{
+		nb.SwitchPorts[lsp.UUID] = &SwitchPort{
 			NB: lsp,
 		}
 	}
-	return switchPorts, nil
+	return nil
 }
 
-func loadRouterPorts(ctx context.Context, cli client.Client) (RouterPorts, error) {
+func (nb *NorthBound) loadRouterPorts(ctx context.Context, cli client.Client) error {
 	lrps := []*nbdb.LogicalRouterPort{}
 	if err := cli.List(ctx, &lrps); err != nil {
-		return nil, err
+		return err
 	}
-	routerPorts := RouterPorts{}
 	for _, lrp := range lrps {
-		routerPorts[lrp.UUID] = RouterPort{
+		port := &RouterPort{
 			NB: lrp,
 		}
+		nb.RouterPorts[lrp.UUID] = port
+		nb.RouterPortByName[lrp.Name] = port
 	}
-	return routerPorts, nil
+	return nil
+}
+
+func (nb *NorthBound) resolveReferences() {
+	for _, sw := range nb.Switches {
+		sw.Ports = SwitchPorts{}
+		for _, portUUID := range sw.NB.Ports {
+			port, ok := nb.SwitchPorts[portUUID]
+			if ok {
+				port.Owner = sw
+				sw.Ports[portUUID] = port
+			}
+		}
+	}
+	for _, rt := range nb.Routers {
+		rt.Ports = RouterPorts{}
+		for _, portUUID := range rt.NB.Ports {
+			port, ok := nb.RouterPorts[portUUID]
+			if ok {
+				port.Owner = rt
+				rt.Ports[portUUID] = port
+			}
+		}
+	}
+	for _, swp := range nb.SwitchPorts {
+		if swp.NB.Options == nil {
+			continue
+		}
+		routerPortName, ok := swp.NB.Options["router-port"]
+		if !ok {
+			continue
+		}
+		routerPort, ok := nb.RouterPortByName[routerPortName]
+		if !ok {
+			continue
+		}
+		swp.RouterPort = routerPort
+	}
+
+	for _, rtp := range nb.RouterPorts {
+		if rtp.NB.Peer == nil {
+			continue
+		}
+		routerName := *rtp.NB.Peer
+		router, ok := nb.Routers[routerName]
+		if !ok {
+			continue
+		}
+		rtp.Peer = router
+	}
 }
